0c6bc54c30e8fdd5b96c760c222525e296e15c16
Aashish0921 <aashish.gupta@xenonstack.com>
2024-08-12 19:50:37 +0530

    uploads

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
+import react from '@astrojs/react';
+export default defineConfig({
+  integrations: [react()],
+  output: 'server',
+  adapter: netlify()
+});
